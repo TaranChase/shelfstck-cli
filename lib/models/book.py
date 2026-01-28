34f8aa73502cdbd08a90496ec5e20e88b0436416
TaranChase <chase.taran@gmail.com>
2026-01-28 21:11:19 +0300

    Created the Book class, added the table title, id and the rest of the necessary columns(title, author, genre, status)

--- lib/models/book.py
+++ lib/models/book.py
@@ -3,3 +3,16 @@ from sqlalchemy import Column, Integer, String
 from . import Base
 
 
+# Book Class 
+
+class Book(Base):
+    __tabletitle__ = "books"
+
+    # Primary Key
+    id = Column(Integer, primary_key=True)
+    
+    title = Column(String)
+    author = Column(String)
+    genre = Column(String)
+    status = Column(String)
+
