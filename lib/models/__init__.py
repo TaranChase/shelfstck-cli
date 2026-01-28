2fe9c87abcbc98d190af9e50998f2e4771482063
TaranChase <chase.taran@gmail.com>
2026-01-28 21:00:23 +0300

    Edited Session to have correct grammar.

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine("sqlite:///shelfstck-cli.db")
-Session = sessionmaker(bindengine)
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
