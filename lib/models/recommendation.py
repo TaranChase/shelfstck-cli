64e859ac99f1fc3b591fb8523ece0f72566a6a0b
TaranChase <chase.taran@gmail.com>
2026-01-29 17:20:19 +0300

    Define recommendation class.

---
+++ lib/models/recommendation.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from . import Base
+
+# Recommendation class
+
+class Recommend(Base):
+    __tablename__ = "recommendations"
+    
+    #Primary Key
+    id = Column(Integer, primary_key=True)
+    note = Column(String)
+
+    # ForeignKey will point ot 'books.id' in books.py
+    book_id = Column(Integer, ForeignKey('books.id'))
+
+    book = relationship("Book", back_populates="recommendations")
