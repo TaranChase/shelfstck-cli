9116d21ba8520471cae8fa5c239424d38411ec6e
TaranChase <chase.taran@gmail.com>
2026-01-29 21:52:34 +0300

    Add helper function to create/add books.

--- lib/helpers.py
+++ lib/helpers.py
@@ -4,6 +4,24 @@ from models import Session, Book, Recommend, Review
 
 session = Session()
 
+# Add Book Logic 
+
+def add_book():
+    book_title = input("Title: ")
+    book_author = input("Author: ")
+    book_genre = input("Genre: ")
+    book_state = input("Status (read/currently reading/completed)")
+
+    if not book_title or not book_author:
+        print("Invalid. Please enter a Book Title and Author.")
+        return
+    
+    book = Book(book_title=book_title, book_author=book_author, book_genre=book_genre, book_state=book_state)
+    session.add(Book)
+    session.commit()
+
+    print("Your book has been added successfully!")
+                
 def helper_1():
     print("Performing useful function#1.")
 
