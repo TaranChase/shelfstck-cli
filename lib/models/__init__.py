1ceb213ce6f8fd0d48f18f7274aa9298dc821a69
TaranChase <chase.taran@gmail.com>
2026-01-29 12:19:03 +0300

    Updated __init__.py file to include the SQLAlchemy 2.0 updated syntax. Changed the base to a modern class-based approach by replacing 'declarative_base()) with 'class Base(DeclarativeBase):

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine("sqlite:///shelfstck-cli.db")
 Session = sessionmaker(bind=engine)
-Base = declarative_base()
+
+class Base (DeclarativeBase):
+    pass
+
 
