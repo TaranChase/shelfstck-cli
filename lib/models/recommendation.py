c36edb6a30504a627c5df45b233d2646a1f357d3
TaranChase <chase.taran@gmail.com>
2026-01-30 18:57:39 +0300

    Add doc string to recommendation.py to document code.

--- lib/models/recommendation.py
+++ lib/models/recommendation.py
@@ -1,10 +1,23 @@
+"""
+Recommendation Model
+
+Creates a recommendation comment associated with a specific book user picks
+
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from . import Base
 
-# Recommendation class
+
 
 class Recommend(Base):
+    """
+    Stores recommendation for a book.
+
+    A recommendation belongs to a book and contains a short comment explaining why the book was recommended
+    """
+    
     __tablename__ = "recommendations"
     
     #Primary Key
