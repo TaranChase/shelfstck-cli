0c89900795d5b98fc613761d7f498ede5b692731
TaranChase <chase.taran@gmail.com>
2026-01-30 19:55:17 +0300

    Add doc strings to book.py to document functions and CRUD methods.

--- lib/models/book.py
+++ lib/models/book.py
@@ -1,4 +1,10 @@
-# Importing database requirements
+"""
+Book Model
+
+Defines Book class and represents its database
+"""
+
+
 from sqlalchemy import Column, Integer, String   
 from . import Base
 from sqlalchemy.orm import relationship
@@ -7,7 +13,15 @@ from sqlalchemy.orm import relationship
 # Book Class 
 
 class Book(Base):
+    """
+    Represents a book in the Shelfstck application
+
+    Book stores the basic necessary data needed i.e title, auhor, genre and state.
+    It maintains relationships between both recommendations and reviews database.
+    """
     __tablename__ = "books"
+
+    # COLUMNS
 
     # Primary Key
     id = Column(Integer, primary_key=True)
@@ -17,30 +31,52 @@ class Book(Base):
     genre = Column(String)
     state = Column(String)
 
-    
-    # Storing a new book in SQLite 
+    # CRUD Methods 
+
     def create(self, session):
+        """
+        Current Book instance is saved to database
+
+        """
+
         session.add(self)
         session.commit()
 
 
-    # View all books
     @classmethod
     def all_books(cls, session):
+        """
+        Returns all books stored in the database
+
+        """
         return session.query(cls).all()
     
 
-    # View specific books using their specific id
     @classmethod
     def find_by_id(cls, session, id):
+        """
+        Finds a book from the database by its unique ID
+        
+        :param session: Active SQLAlchemy session
+        :param id: Unique ID of a book to be retrieved from database
+        """
+
         return session.query(cls).filter_by(id=id).first()
     
-    # Deleting/Removing a book 
+
     def delete(self, session):
+        """
+        Deletes and Removes the current Book instance from the database
+
+        The associated recommendations and reviews are also deleted
+        """
+
         session.delete(self)
         session.commit()
     
-    # Updating Book to complete and sync the relationship
+
+    # RELATIONSHIPS 
+
     recommendations = relationship("Recommend", back_populates="book", cascade="all, delete")
     reviews = relationship("Review", back_populates="book", cascade="all, delete")
 
