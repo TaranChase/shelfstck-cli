2e6b410fe28c3b3a9a2d944f82ba2c1e80d68e0b
TaranChase <chase.taran@gmail.com>
2026-01-30 19:05:11 +0300

    Add doc string to review.py to document code.

--- lib/models/review.py
+++ lib/models/review.py
@@ -1,10 +1,22 @@
+"""
+Review Model
+
+Creates a numeric review (1-5) for a book
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from . import Base
 
-# Define review class
+
 
 class Review(Base):
+    """
+    Stores a review made by user for a book
+
+    A review is a numeric rating between 1 to 5 linked to a single book
+    """
+    
     __tablename__ = "reviews"
 
     id = Column(Integer, primary_key=True)
