14b3d91a1b9d9f6830e7ce9e2d992ae626eaa75c
TaranChase <chase.taran@gmail.com>
2026-01-30 20:09:37 +0300

    Add doc string to __init__.py to document Model Package Initialization and Importation of models.

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,3 +1,12 @@
+"""
+Initializing models package
+
+This configures the database and ORM for the Shelfstck CLI Application 
+It includes the SQLAlchemy engine, session maker, and the Declarative Base class used by all the models
+
+The model classes are imported here to ensure that they are registered with the SQLAlchemy metadata.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
@@ -5,10 +14,13 @@ engine = create_engine("sqlite:///shelfstck-cli.db")
 Session = sessionmaker(bind=engine)
 
 class Base (DeclarativeBase):
+    """
+    The Base class for all SQLAlchemy ORM Models
+    """
     pass
 
 
-# Importing model
+# Importing models to register with SQLAlchemy
 from .book import Book
 from .recommendation import Recommend
 from .review import Review
